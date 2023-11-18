74effbb557deecdb8c66d5323aec318e987d6468
divya <divysg43@gmail.com>
2023-11-18 19:43:25 +0530

    added content

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React divya is a good abg
         </a>
       </header>
     </div>
